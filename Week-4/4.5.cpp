b7a7e73767528f14fd958fcdfb7e0a5475201d69
Riddhima Singh <85569566+zany1105@users.noreply.github.com>
2026-08-29 23:20:37 +0530

    Create 4.5.cpp

---
+++ Week-4/4.5.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> mergeSortedArrays(vector<int>& arr1, vector<int>& arr2) {
+    vector<int> result;
+
+    int i = 0;
+    int j = 0;
+
+    while (i < arr1.size() && j < arr2.size()) {
+        if (arr1[i] < arr2[j]) {
+            result.push_back(arr1[i]);
+            i++;
+        }
+        else {
+            result.push_back(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.size()) {
+        result.push_back(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.size()) {
+        result.push_back(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> arr1 = {1, 3, 5};
+    vector<int> arr2 = {2, 4, 6};
+
+    vector<int> result = mergeSortedArrays(arr1, arr2);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
