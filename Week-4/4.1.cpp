a01bc22b7e132def802e42769a9e088cd70234cd
Riddhima Singh <85569566+zany1105@users.noreply.github.com>
2026-08-29 23:14:41 +0530

    Implement twoSum function in 4.1.cpp

---
+++ Week-4/4.1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j};
+            }
+        }
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = twoSum(nums, target);
+
+    cout << "[" << ans[0] << ", " << ans[1] << "]";
+
+    return 0;
+}
